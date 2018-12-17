5d39a73ea900cb4a775ce67fcc17026ffd32c8b4
Rishi Prasad <rishipr@umich.edu>
2018-12-16 23:45:33 -0500

    Updated button layout on Landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,29 +17,33 @@ class Landing extends Component {
               passport and JWTs
             </p>
             <br />
-            <Link
-              to="/register"
-              style={{
-                width: "140px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px"
-              }}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Register
-            </Link>
-            <Link
-              to="/login"
-              style={{
-                marginLeft: "2rem",
-                width: "140px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px"
-              }}
-              className="btn btn-large btn-flat waves-effect white black-text"
-            >
-              Log In
-            </Link>
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  marginLeft: "2rem",
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large btn-flat waves-effect white black-text"
+              >
+                Log In
+              </Link>
+            </div>
           </div>
         </div>
       </div>
