c2e420dace42d7576fb9ddd99dfd8d8154d5dc29
KT-Data <devin.ramsammy@khanstutorial.com>
2019-11-09 14:25:43 -0500

    changed keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
+  mongoURI: "mongodb+srv://Naveed:<password>@cluster0-c6c3a.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
