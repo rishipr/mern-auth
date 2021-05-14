009071ef83cc1df665146754d8bc422df7807289
Josh Levy <59902856+jml312@users.noreply.github.com>
2021-05-14 18:17:31 -0500

    Remove deprecated body-parser

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
 
 const app = express();
-
-// Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
