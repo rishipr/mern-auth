d30dd0fb190e07aa763fb53d3656cb8221638c60
Swamy <meeraswamy555@gmail.com>
2023-04-01 20:12:24 +0530

    Added docker files and updated README

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-
+const cors = require("cors");
 const users = require("./routes/api/users");
 
 const app = express();
 
+app.use(cors());
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
